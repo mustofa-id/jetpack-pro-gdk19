e65e8f7c02160e9a9ee0cc4fc918ee38b877aa6e
hm <hm.dev7@gmail.com>
2019-08-07 09:54:04 +0700

    Using Glide module

--- AmberMovie/src/main/java/id/mustofa/app/amber/util/Extension.kt
+++ AmberMovie/src/main/java/id/mustofa/app/amber/util/Extension.kt
@@ -1,6 +1,7 @@
 package id.mustofa.app.amber.util
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.view.View
 import android.widget.ImageView
@@ -8,7 +9,7 @@ import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
-import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.module.AppGlideModule
@@ -23,7 +24,21 @@ import kotlin.reflect.KClass
  */
 
 @GlideModule
-class ImageModule : AppGlideModule()
+class ImageModule : AppGlideModule() {
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+        val loader = CircularProgressDrawable(context).apply {
+            strokeWidth = 3f
+            centerRadius = 32f
+            start()
+        }
+        val options = RequestOptions()
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+            .placeholder(loader)
+            .error(R.drawable.ic_image_error)
+        builder.setDefaultRequestOptions(options)
+    }
+}
 
 fun Activity.toActivity(clz: KClass<*>, extras: ((Intent) -> Unit)? = null) {
     val intent = Intent(applicationContext, clz.java)
@@ -60,17 +75,6 @@ fun Activity.snackIt(
 }
 
 fun <T> ImageView.load(image: T) {
-    val context = rootView.context
-    val loader = CircularProgressDrawable(context).apply {
-        strokeWidth = 5f
-        centerRadius = 30f
-        backgroundColor = R.color.accent
-        start()
-    }
-    val options = RequestOptions()
-        .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
-        .placeholder(loader)
-        .error(R.drawable.ic_image_error)
-
-    Glide.with(context).load(image).apply(options).into(this)
+    GlideApp.with(rootView)
+        .load(image).into(this)
 }
