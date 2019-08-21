067c32345f993710c1aabd5b309a132346073807
hm <hm.dev7@gmail.com>
2019-08-21 10:14:40 +0700

    Rename & improve performance

--- AmberMovie/src/main/java/id/mustofa/app/amber/ui/movie/MovieAdapter.kt
+++ AmberMovie/src/main/java/id/mustofa/app/amber/ui/movie/MovieAdapter.kt
@@ -1,8 +1,8 @@
 package id.mustofa.app.amber.ui.movie
 
-import android.view.ViewGroup
+import android.view.View
 import id.mustofa.app.amber.R
-import id.mustofa.app.amber.base.BaseRecyclerAdapter
+import id.mustofa.app.amber.base.SimpleRecyclerAdapter
 import id.mustofa.app.amber.data.Movie
 import id.mustofa.app.amber.util.loadTmdbImage
 import kotlinx.android.synthetic.main.item_movie.view.*
@@ -11,18 +11,16 @@ import kotlinx.android.synthetic.main.item_movie.view.*
  * @author Habib Mustofa
  * Indonesia on 05/08/19
  */
-class MovieAdapter : BaseRecyclerAdapter<Movie>() {
+class MovieAdapter : SimpleRecyclerAdapter<Movie>(R.layout.item_movie) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        return object : Holder(inflate(parent, R.layout.item_movie)) {
+    override fun getViewHolder(view: View) = MovieViewHolder(view)
 
-            override fun setItem(item: Movie) {
-                super.setItem(item)
-                itemView.run {
-                    imgItemMoviePoster.loadTmdbImage(item.posterPath)
-                    textItemMovieRating.text = item.voteAverage.toString()
-                    textItemMovieTitle.text = item.title
-                }
+    inner class MovieViewHolder(view: View) : Holder(view) {
+        override fun setItem(item: Movie) {
+            itemView.run {
+                imgItemMoviePoster.loadTmdbImage(item.posterPath)
+                textItemMovieRating.text = item.voteAverage.toString()
+                textItemMovieTitle.text = item.title
             }
         }
     }
--- AmberMovie/src/main/java/id/mustofa/app/amber/ui/tvshow/TvshowAdapter.kt
+++ AmberMovie/src/main/java/id/mustofa/app/amber/ui/tvshow/TvshowAdapter.kt
@@ -1,8 +1,8 @@
 package id.mustofa.app.amber.ui.tvshow
 
-import android.view.ViewGroup
+import android.view.View
 import id.mustofa.app.amber.R
-import id.mustofa.app.amber.base.BaseRecyclerAdapter
+import id.mustofa.app.amber.base.SimpleRecyclerAdapter
 import id.mustofa.app.amber.data.Movie
 import id.mustofa.app.amber.util.loadTmdbImage
 import kotlinx.android.synthetic.main.item_tvshow.view.*
@@ -11,17 +11,16 @@ import kotlinx.android.synthetic.main.item_tvshow.view.*
  * @author Habib Mustofa
  * Indonesia on 05/08/19
  */
-class TvshowAdapter : BaseRecyclerAdapter<Movie>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        return object : Holder(inflate(parent, R.layout.item_tvshow)) {
+class TvshowAdapter : SimpleRecyclerAdapter<Movie>(R.layout.item_tvshow) {
 
-            override fun setItem(item: Movie) {
-                super.setItem(item)
-                itemView.run {
-                    imgItemTvshowPoster.loadTmdbImage(item.posterPath)
-                    textItemTvshowRating.text = String.format("%.1f", item.voteAverage)
-                    textItemTvshowTitle.text = item.title
-                }
+    override fun getViewHolder(view: View) = TvshowViewHolder(view)
+
+    inner class TvshowViewHolder(view: View) : Holder(view) {
+        override fun setItem(item: Movie) {
+            itemView.run {
+                imgItemTvshowPoster.loadTmdbImage(item.posterPath)
+                textItemTvshowRating.text = String.format("%.1f", item.voteAverage)
+                textItemTvshowTitle.text = item.title
             }
         }
     }
